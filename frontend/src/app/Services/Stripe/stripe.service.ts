060db36085a5798eab6844755a38df575083c0a1
Salvatore De Rosa <74751551+maidastach@users.noreply.github.com>
2022-03-11 10:10:39 +1100

    Update stripe.service.ts

--- frontend/src/app/Services/Stripe/stripe.service.ts
+++ frontend/src/app/Services/Stripe/stripe.service.ts
@@ -5,7 +5,7 @@ import { Order } from 'src/app/Interfaces/Order';
 import { Shipping } from 'src/app/Interfaces/User';
 import { StripeService } from 'ngx-stripe';
 
-export const stripePublicKey: string = 'pk_live_51ISWUsIJZPLIB8kIUazKZV3cZeC1uFzZLJzHO0MZBN0p9LUf3i8roD6IRnmI4L7r6aH4FRY4sI2Qgcd0ZJyhk73s00l37UWGf2';
+export const stripePublicKey: string = '****************************************';
   
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,7 @@ export class MyStripeService
 
   checkout(id: Order['_id'], shipping: Shipping | null | undefined)
   {
-    return this.http.post<{ id: any }>(`/api/stripe/create-checkout-session/${id}`, shipping)
+    return this.http.post<{ id: any }>(`/**************************`, shipping)
       .pipe(
         switchMap(
           session => this.stripeService.redirectToCheckout({ sessionId: session.id })
